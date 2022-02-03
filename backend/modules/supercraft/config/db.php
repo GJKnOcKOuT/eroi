549bc6b6d75fddc9705a65cf119c96b9f089a229
GJKnOcKOuT <aldobarabba2000@gmail.com>
2022-02-03 15:48:57 +0100

    attempt to connect to the db

--- backend/modules/supercraft/config/db.php
+++ backend/modules/supercraft/config/db.php
@@ -1,11 +1,14 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mydb',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=mydb',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        'enableSchemaCache' => false,
+        'schemaCacheDuration' => 88000,
+        'schemaCache' => 'schemaCache',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
